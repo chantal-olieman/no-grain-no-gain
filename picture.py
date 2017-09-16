815aff6cc6eb525e75b5840c665a6868ca4b2fac
Noor <Noor@MacBook-Pro-van-alex.local>
2017-09-16 16:31:07 +0200

    Mijn fout alweer

--- picture.py
+++ picture.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image
-imgs = Image.open("img/testweinig.jpeg")
+imgs = Image.open("img/testveel.jpeg")
 #print imgs.size
 #print im.size
 #im = imgs.resize((200, 300), Image.ANTIALIAS)
 #im.show()
 arr = np.array(imgs)
 #print np.size(arr)
-img = Image.fromarray(arr[350:850,0:500], 'RGB')
+img = Image.fromarray(arr[0:1400,0:1400], 'RGB')
 img.show()
 img.save('img/testveel2.jpeg')
