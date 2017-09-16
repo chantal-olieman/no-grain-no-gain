ee1a1312bae41767b0e35cde0d1695471348c52d
Noor <Noor@MacBook-Pro-van-alex.local>
2017-09-16 16:09:38 +0200

    meeer

--- picture.py
+++ picture.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image
-imgs = Image.open("img/testveel.jpeg")
+imgs = Image.open("img/stone.jpeg")
 #print imgs.size
 #print im.size
 #im = imgs.resize((200, 300), Image.ANTIALIAS)
 #im.show()
 arr = np.array(imgs)
 #print np.size(arr)
-img = Image.fromarray(arr[800:2200,1500:3000], 'RGB')
+img = Image.fromarray(arr[2000:3000,0:3000], 'RGB')
 img.show()
-img.save('img/testveel.jpeg')
+#img.save('img/graan2.jpeg')
