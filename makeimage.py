24dea64fbf97007c183e0035fa93c3b9e039df79
Noor <Noor@MacBook-Pro-van-alex.local>
2017-09-16 15:19:17 +0200

    pic.jpeg

--- makeimage.py
+++ makeimage.py
@@ -1,15 +1,16 @@
-def merge_images():
-    from PIL import Image
-    image1 = Image.open("img/graan.jpeg")
-    image2 = Image.open("img/red.jpeg")
+from PIL import Image
+import numpy
+image1 = Image.open("img/graan.jpeg")
+image2 = Image.open("img/red.jpeg")
     
 (width1, height1) = image1.size
 (width2, height2) = image2.size
 
 resw = width1 + width2
-resh = max(height1 + heigth2)
+#resh = numpy.max(height1,height2)
 
-result = Image.new('RGB', (resw, resh))
+result = Image.new('RGB', (resw, height1))
 result.paste(im=image1, box=(0,0))
 result.paste(im=image2, box=(width1, 0))
-print result
+#result.show()
+result.save('img/pic.jpeg')
