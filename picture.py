c75205b75f0c9ec82418b501f1f849c3f24da825
Noor <Noor@MacBook-Pro-van-alex.local>
2017-09-16 16:24:04 +0200

    Nog meer plaatjes

--- picture.py
+++ picture.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image
-imgs = Image.open("img/stone.jpeg")
+imgs = Image.open("img/testweinig.jpeg")
 #print imgs.size
 #print im.size
 #im = imgs.resize((200, 300), Image.ANTIALIAS)
 #im.show()
 arr = np.array(imgs)
 #print np.size(arr)
-img = Image.fromarray(arr[2000:3000,0:3000], 'RGB')
+img = Image.fromarray(arr[900:1900,2000:2500], 'RGB')
 img.show()
-#img.save('img/graan2.jpeg')
+img.save('img/testweinig.jpeg')
